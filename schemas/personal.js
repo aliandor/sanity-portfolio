738589de50de52ceeaaa292f39835ac180629c8c
Alfonso Galang <alfonsogalang86@gmail.com>
2019-09-05 09:59:46 -0700

    added personal project schema

--- schemas/personal.js
+++ schemas/personal.js
@@ -1,16 +1,11 @@
 export default {
-  name: "project",
-  title: "Project",
+  name: "personal",
+  title: "Personal Project",
   type: "document",
   fields: [
     {
       name: "projectName",
       title: "Project Name",
-      type: "string"
-    },
-    {
-      name: "projectType",
-      title: "Project Type",
       type: "string"
     },
     {
@@ -43,50 +38,9 @@ export default {
       type: "text"
     },
     {
-      name: "role",
-      title: "Role",
-      type: "string"
-    },
-    {
-      name: "date",
-      title: "Date",
-      type: "string"
-    },
-    {
-      name: "concept",
-      title: "Concept",
-      type: "text"
-    },
-    {
-      name: "tech",
-      title: "Tech",
-      type: "array",
-      of: [{ type: "image" }]
-    },
-    {
       name: "responsiveImage",
       title: "Responsive Image",
       type: "image"
-    },
-    {
-      name: "challenge",
-      title: "The Challenge",
-      type: "text"
-    },
-    {
-      name: "solution",
-      title: "The Solution",
-      type: "text"
-    },
-    {
-      name: "codeImage",
-      title: "Code Image",
-      type: "image"
-    },
-    {
-      name: "color",
-      title: "Theme Color",
-      type: "string"
     },
     {
       name: "urlSite",
